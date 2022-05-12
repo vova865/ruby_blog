4152e3ea8a2c4d616b8edc388b0cef2e89a3a720
Vova <vova.com940@gmail.com>
2022-05-12 11:41:14 +0300

    fix bugs

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -22,7 +22,7 @@ class Users::SessionsController < Devise::SessionsController
     new_user_session_path
   end
 
-  def after_sign_in_path_for(_resource_or_scope)
+  def after_sign_in_path_for(resource_or_scope)
     stored_location_for(resource_or_scope) || root_path
   end
   # protected
