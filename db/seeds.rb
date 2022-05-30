0069d81996ca79f2fdc87c32002d109d47bb4ac0
Vova <vova.com940@gmail.com>
2022-05-30 18:17:26 +0300

    switch to pg

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,11 @@
 20.times do
   title = Faker::Hipster.word
   body = Faker::Hipster.paragraph_by_chars(characters: 500, supplemental: false)
-  user_id = 5
-  category_id = 1
-  Article.create title: title, body: body, user_id: user_id, category_id: category_id
+  user_id = 1
+  Article.create title: title, body: body, user_id: user_id
 end
 
-if Rails.env.development?
-  AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
-end
+# if Rails.env.development?
+#   AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
+# end
 
